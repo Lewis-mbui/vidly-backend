702e9ad1a05975130c50a07f14d4da57e15ef769
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-18 10:19:10 +0300

    build service for getting genres

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Comedy" },
+  { id: 3, name: "Drama" },
+  { id: 4, name: "Horror" },
+];
+
+
 app.get('/', (req, res) => {
   res.send('Welcome to vidly!');
 })
+
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
