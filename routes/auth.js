91ed3466d260c117028fdd86b083cf7bf0520c17
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-25 16:25:53 +0300

    generate jwt for logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const Joi = require('Joi');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
@@ -16,8 +17,9 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password');
 
-  res.send(true);
-})
+  const token = jwt.sign({_id: user._id}, 'jwtPrivateKey');
+  res.send(token);
+});
 
 function validate(req) {
   const schema = Joi.object({
