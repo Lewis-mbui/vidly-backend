81c66e2a2bf5a5319cd1e6006fafb40ad5a8c8c1
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-25 18:50:11 +0300

    set response headers when creating a user

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const {validate, User} = require('../models/user');
@@ -17,7 +18,8 @@ router.post('/', async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send( _.pick(user, ['_id', 'name', 'email']));
+  const token = jwt.sign({_id: user._id}, process.env.JWT_PRIVATE_KEY);
+  res.header('x-auth-token', token).send( _.pick(user, ['_id', 'name', 'email']));
 })
 
 module.exports = router;
