77a145bd5abc60c8c9b52f52a5d75a3ce80ea2dc
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-26 16:11:46 +0300

    remove try-catch blocks

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const asyncMiddleware = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const {Genre, validate} = require('../models/genre');
@@ -5,15 +6,10 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  try {
+router.get('/', asyncMiddleware(async (req, res, next) => {
     const genres = await Genre.find().sort('name');
     res.send(genres);
-  }
-  catch (ex) {
-    next(ex);
-  }
-});
+}));
 
 router.post('/', auth, async (req, res) => {
   const {error} = validate(req.body);
@@ -24,7 +20,7 @@ router.post('/', auth, async (req, res) => {
   res.send(genre);
 });
 
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', auth, asyncMiddleware(async (req, res) => {
   const {error} = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -35,22 +31,22 @@ router.put('/:id', auth, async (req, res) => {
   if (!genre) return res.status(404).send('Could not find genre with given id');
 
   res.send(genre);
-});
+}));
 
-router.delete('/:id', [auth, admin], async (req, res) => {
+router.delete('/:id', [auth, admin], asyncMiddleware(async (req, res) => {
   const genre = await Genre.findByIdAndDelete(req.params.id);
 
   if (!genre) return res.status(404).send('Could not find genre with given id');
 
   res.send(genre);
-});
+}));
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', asyncMiddleware(async (req, res) => {
   const genre = await Genre.findById(req.params.id);
 
   if (!genre) return res.status(404).send('Could not find genre with given id');
 
   res.send(genre);
-});
+}));
 
 module.exports = router;
