96d79ba3a3c69a0100753034efcbddd6ac0e18c3
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-26 10:40:19 +0300

    build endpoint for getting current user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+const auth = require('../middleware/auth');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const {validate, User} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+
+router.get('/me', auth, async(req, res) => {
+  const user = await User.findById(req.user._id)
+    .select('-password');
+    res.send(user);
+})
 
 router.post('/', async (req, res) => {
   const {error} = validate(req.body);
