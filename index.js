632bc4a9ade2b374164c46201a5364702cfd1f11
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-27 09:03:54 +0300

    Refactor: extract db logic

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ require('winston-mongodb');
 require('dotenv').config();
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-
-require('./routes/auth');
 const express = require('express');
 const app = express();
+
 require('./startup/routes')(app);
+require('./startup/db')();
 
 winston.exceptions.handle(new winston.transports.File({filename: 'uncaughtExceptions.log'}));
 winston.rejections.handle(new winston.transports.File({filename: 'unhandledRejections.log'}));
@@ -28,19 +27,12 @@ winston.add(new winston.transports.MongoDB({
   level: 'info'
 }));
 
-// throw new Error('Something failed during startup.');
-
-const p = Promise.reject(new Error('Something failed miserably!!'));
-p.then(() => console.log('Done'));
-
 if (!process.env.JWT_PRIVATE_KEY) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
 }
 
-mongoose.connect('mongodb://127.0.0.1/vidly')
-  .then(() => console.log('connected to MongoDb...'))
-  .catch(err => console.error('Could not connect to MongoDB'));
+
 
 
 
