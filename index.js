75306636e95e6094bd4c849c79b09d3072cf30f4
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-27 09:54:53 +0300

    Refactor: extract logging logic

--- index.js
+++ index.js
@@ -1,41 +1,20 @@
-const winston = require('winston');
-require('winston-mongodb');
+
 require('dotenv').config();
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 
-winston.exceptions.handle(new winston.transports.File({filename: 'uncaughtExceptions.log'}));
-winston.rejections.handle(new winston.transports.File({filename: 'unhandledRejections.log'}));
 
-
-winston.add(new winston.transports.File({
-  filename: 'logfile.log',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  )
-})
-);
-
-winston.add(new winston.transports.MongoDB({
-  db: 'mongodb://127.0.0.1/vidly',
-  level: 'info'
-}));
 
 if (!process.env.JWT_PRIVATE_KEY) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
 }
-
-
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ startup/logging.js
@@ -0,0 +1,27 @@
+const winston = require('winston');
+require('winston-mongodb');
+
+module.exports = function() {
+  winston.exceptions.handle(new winston.transports.File({filename: 'uncaughtExceptions.log'}));
+  winston.rejections.handle(new winston.transports.File({filename: 'unhandledRejections.log'}));
+
+  winston.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  }));
+
+  winston.add(new winston.transports.File({
+    filename: 'logfile.log',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    )
+  }));
+
+  winston.add(new winston.transports.MongoDB({
+    db: 'mongodb://127.0.0.1/vidly',
+    level: 'info'
+  }));
+}
