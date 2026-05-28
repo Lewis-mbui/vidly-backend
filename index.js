0158166888c569e9158dad7f679939a87b9cf65b
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-28 09:19:28 +0300

    TEST: first unit test

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-console.log(process.env.NODE_ENV);
-require('./startup/config')();
 const winston = require('winston');
 const express = require('express');
 const app = express();
 
+require('./startup/config')();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-require('./startup/config')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   winston.info(`server running on port ${port}...`);
 });
+
+module.exports = server;
---
+++ tests/integration/genres.test.js
@@ -0,0 +1,20 @@
+// SET UP ENVIRONMENT VARIABLES
+require('../../startup/config')();
+
+const request = require('supertest');
+let server;
+
+describe('/api/genres', () => {
+  beforeEach(() => {
+    server = require('../../index');
+  });
+
+  afterEach(() => {server.close(); })
+
+  describe('GET /', () => {
+    it('should return all genres', async () => {
+      const res = await request(server).get('/api/genres');
+      expect(res.status).toBe(200);
+    })
+  })
+});
