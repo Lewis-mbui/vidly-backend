8808314337f5d9c52f31e7ce7a98148d12c3f039
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-28 07:01:14 +0300

    TEST: write unit test for user.generatAuthToken

---
+++ tests/unit/models/user.test.js
@@ -0,0 +1,24 @@
+const {User} = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+describe('user.generateAuthToken', () => {
+   beforeEach(() => {
+    process.env.JWT_PRIVATE_KEY = 'testPrivateKey';
+  });
+
+  afterEach(() => {
+    delete process.env.JWT_PRIVATE_KEY;
+  });
+
+  it('should return a valid JWT', () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(), 
+      isAdmin: true
+    };
+    const user = new User(payload);
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+    expect(decoded).toMatchObject(payload);
+  });
+});
