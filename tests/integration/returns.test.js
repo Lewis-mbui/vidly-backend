1b9d8deb517d6f373b3f15856926eb861cbafdd3
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-29 15:14:28 +0300

    TEST: testing the response

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -133,4 +133,14 @@ describe('/api/returns', () => {
 
     expect(movieInDb.numberInStock).toBe(movie.numberInStock + 1);
   });
+  
+  it('should return the rental if input is valid', async () => {
+    const res = await exec();
+
+    const rentalInDb = await Rental.findById(rental._id);
+
+    expect(Object.keys(res.body)).toEqual(
+      expect.arrayContaining(['dateOut', 'dateReturned', 'rentalFee', 'customer', 'movie'])
+    );
+  });
 });
