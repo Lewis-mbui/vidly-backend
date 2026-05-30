70818f9a579130e0af581cb253edc3992224e883
Lewis-mbui <lewmugo14479@gmail.com>
2026-05-30 16:53:33 +0300

    fix: edit log files and db connstr

--- startup/db.js
+++ startup/db.js
@@ -5,5 +5,5 @@ const config = require('config');
 module.exports = function() {
   const db = process.env.db || config.get('db');
   mongoose.connect(db)
-  .then(() => winston.info(`connected to ${db}...`));
+  .then(() => winston.info(`connected to database...`));
 }
